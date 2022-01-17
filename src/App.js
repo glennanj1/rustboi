3af8a139fe7cbdc0aa6c295d5cae7efc21eb055b
John Glennan <61764500+glennanj1@users.noreply.github.com>
2022-01-17 13:05:34 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome To Rustboi's Official</h1>
         <h2>Fresh wipe every Thursday</h2>
-        <h2>Last Wipe 01/13/2022</h2>
-        <h3>10X Server</h3>
-        <h3>&</h3>
-        <h3>2X Server</h3>
+        <h2>Last Wipe 01/16/2022</h2>
+        <h3>10X Server & 2X Server</h3>
         <button><a href="https://discord.gg/t6umrsMWSq" title="discord">Discord</a></button>
       </header>
     </div>
