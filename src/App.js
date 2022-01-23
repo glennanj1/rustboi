6e464499714c3ec9f8e9b446a07bb566094b0621
john glennan <glennan20@gmail.com>
2022-01-23 16:29:47 -0500

    adding model to home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import RustAKModelFinal from './RustAKModelFinal'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Canvas camera={{zoom: 100, position: [0, 0, 100]}}>
+          <Suspense fallback={null}>
+            <ambientLight intensity={1} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <RustAKModelFinal />
+          </Suspense>
+        </Canvas>  
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Welcome To Rustboi's Official</h1>
         <h2>Fresh wipe every Thursday</h2>
         <h2>Last Wipe 01/16/2022</h2>
         <h3>10X Server & 2X Server</h3>
         <button><a href="https://discord.gg/t6umrsMWSq" title="discord">Discord</a></button>
-      </header>
+        <Canvas camera={{zoom: 100, position: [0, 0, 100]}}>
+          <Suspense fallback={null}>
+            <ambientLight intensity={1} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <RustAKModelFinal />
+          </Suspense>
+        </Canvas>  
+       </header>
     </div>
   );
 }
